28c866831dc6032870a4b693ab4c673759a944eb
Chris Huckins <chris.huckins@tlgcohort.com>
2021-04-12 18:50:39 -0400

    lab01 updated fo junit

--- Lab01/src/Main.java
+++ Lab01/src/Main.java
@@ -1,12 +1,20 @@
+import java.util.regex.*;
+
 class Main {
     public static void main(String[] args) {
         System.out.println(convertBinToDecimal("111"));
         System.out.println(convertOctToDecimal("301"));
         System.out.println(convertOctToDecimal_recur("301",2));
-        System.out.println(convertOctToDecimalUsingRecursion("301"));
+        System.out.println(convertOctToDecimalUsingRecursion("0301"));
     }
 
     static public int convertBinToDecimal(String binary) {
+        Pattern p = Pattern.compile("^[0-1][0-1]*$");
+        Matcher m = p.matcher(binary);
+        //System.out.println(m.matches());
+        if (!m.matches()){
+            throw new IllegalArgumentException("String provided in not an valid binary value");
+        }
         int conversion = 1;
         int result = 0;
         for (int i = 1; i <= binary.length(); i++){
@@ -31,16 +39,21 @@ class Main {
 
     static public int convertOctToDecimal_recur(String oct, int exp) {
         int digit = Integer.parseInt(String.valueOf(oct.charAt(oct.length() - (exp+1))));
-        int converstionValue = (int) Math.pow(8,exp);
+        int conversionValue = (int) Math.pow(8,exp);
         if (exp == 0) {
             return digit;
         }
         else {
-            return digit * converstionValue + convertOctToDecimal_recur(oct, exp -1);
+            return digit * conversionValue + convertOctToDecimal_recur(oct, exp -1);
         }
     }
 
-    static public int convertOctToDecimalUsingRecursion(String oct){
+    static public int convertOctToDecimalUsingRecursion(String oct) throws IllegalArgumentException{
+        Pattern p = Pattern.compile("^[0-7][0-7]*$");
+        Matcher m = p.matcher(oct);
+        if (!m.matches()){
+            throw new IllegalArgumentException("String provided in not an valid oct value");
+        }
         return convertOctToDecimal_recur(oct, oct.length()-1);
     }
 }
--- Lab01/test/MainTest.java
+++ Lab01/test/MainTest.java
@@ -1,34 +1,84 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class MainTest {
+public class MainTest {
+    @Test
+    public void conBin_0_shouldReturn_0() {
+        assertEquals(0,Main.convertBinToDecimal("0"));
+    }
 
     @Test
-    void test1(){
-        assertEquals(7,Main.convertBinToDecimal("111"));
-        assertEquals(3,Main.convertBinToDecimal("11"));
+    public void conBin_1_shouldReturn_1() {
         assertEquals(1,Main.convertBinToDecimal("1"));
     }
 
     @Test
-    void test2(){
-        assertEquals(1,Main.convertOctToDecimal("1"));
-        assertEquals(9,Main.convertOctToDecimal("11"));
-        assertEquals(17,Main.convertOctToDecimal("21"));
-        assertEquals(81,Main.convertOctToDecimal("121"));
-        assertEquals(302,Main.convertOctToDecimal("456"));
-        assertEquals(19246,Main.convertOctToDecimal("45456"));
+    public void conBin_10_shouldReturn_2() {
+        assertEquals(2,Main.convertBinToDecimal("10"));
+    }
+    @Test
+    public void conBin_1001_shouldReturn_9() {
+        assertEquals(9,Main.convertBinToDecimal("1001"));
     }
 
     @Test
-    void test3(){
-        assertEquals(1,Main.convertOctToDecimalUsingRecursion("1"));
-        assertEquals(9,Main.convertOctToDecimalUsingRecursion("11"));
-        assertEquals(17,Main.convertOctToDecimalUsingRecursion("21"));
-        assertEquals(81,Main.convertOctToDecimalUsingRecursion("121"));
-        assertEquals(302,Main.convertOctToDecimalUsingRecursion("456"));
-        assertEquals(19246,Main.convertOctToDecimalUsingRecursion("45456"));
+    public void conBin_11111111_shouldReturn_255() {
+        assertEquals(255,Main.convertBinToDecimal("11111111"));
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void conBin_acb_shouldThrowException() {
+        Main.convertBinToDecimal("abc");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void conBin_123_shouldThrowException() {
+        Main.convertBinToDecimal("123");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void conBin_1F_shouldThrowException() {
+        Main.convertBinToDecimal("1F");
+    }
+
+    @Test
+    public void conOct_0_shouldReturn_0() {
+        assertEquals(0,Main.convertOctToDecimalUsingRecursion("0"));
+    }
+
+    @Test
+    public void conOct_1_shouldReturn_1() {
+        assertEquals(1,Main.convertOctToDecimalUsingRecursion("1"));
+    }
+
+    @Test
+    public void conOct_10_shouldReturn_8() {
+        assertEquals(8,Main.convertOctToDecimalUsingRecursion("10"));
+    }
+
+    @Test
+    public void conOct_654_shouldReturn_10() {
+        assertEquals(428,Main.convertOctToDecimalUsingRecursion("654"));
+    }
+
+    @Test
+    public void conOct_701654_shouldReturn_230316() {
+        assertEquals(230316,Main.convertOctToDecimalUsingRecursion("701654"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void conOct_814_shouldThrowException() {
+        Main.convertOctToDecimalUsingRecursion("814");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void conOct_abc_shouldThrowException() {
+        Main.convertOctToDecimalUsingRecursion("abc");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void conOct_1A_shouldThrowException() {
+        Main.convertOctToDecimalUsingRecursion("1A");
+    }
 }
